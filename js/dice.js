51ee8360e5fea7b5817806bfccffcd45041e0a02
rael gesaka <raelmoraa85@gmail.com>
2018-09-28 18:01:10 +0300

    Add html file,stylesheets and image and their effect

---
+++ js/dice.js
@@ -0,0 +1,8 @@
+var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
+
+        // Display result
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        //logic
+        if roll1
